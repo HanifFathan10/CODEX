2e379a8b8fe5bbd8535cf2435fc910a8328e36b5
Hanif Fathan <haniffathan33@gmail.com>
2026-02-03 16:08:21 +0700

    refactor: js switcher

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { GoogleGenAI, ThinkingLevel } from "@google/genai";
-// import sqlite3 from "sqlite3";
-// import path from "path";
-// import { fileURLToPath } from "url";
+import profile from "./data.json" with { type: "json" };
 
 dotenv.config();
 
@@ -14,83 +12,19 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-// DATABASE
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const dbPath = path.resolve(__dirname, "persona.db");
-// const db = new sqlite3.Database(dbPath, (err) => {
-//   if (err) {
-//     console.error("Gagal menyambung ke database:", err.message);
-//   } else {
-//     console.log("Terhubung ke database SQLite.");
-//     initializeDatabase();
-//   }
-// });
-
-// function initializeDatabase() {
-//   const query = `
-//         CREATE TABLE IF NOT EXISTS person (
-//             id INTEGER PRIMARY KEY AUTOINCREMENT,
-//             name VARCHAR(100) NOT NULL,
-//             role VARCHAR(100) NOT NULL,
-//             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-//             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-//         )
-//     `;
-//   db.run(query, (err) => {
-//     if (err) {
-//       console.error("Gagal membuat tabel:", err.message);
-//     } else {
-//       console.log("Database + tabel siap digunakan.");
-//     }
-//   });
-// }
-
 // GEN AI
 const genAI = new GoogleGenAI({
   apiKey: process.env.SECRET_KEY_GEMINI_API,
 });
 
 // API
-// GET
-// app.get("/api/person", (req, res) => {
-//   const sql = "SELECT * FROM person";
-//   db.all(sql, [], (err, rows) => {
-//     if (err) {
-//       return res.status(500).json({ error: err.message });
-//     }
-//     res.json({
-//       message: "Berhasil mengambil semua produk",
-//       data: rows,
-//     });
-//   });
-// });
-
-// CREATE
-// app.post("/api/person", (req, res) => {
-//   const { name, role } = req.body;
-
-//   if (!name || !role) {
-//     return res.status(400).json({ error: "Nama dan role wajib diisi" });
-//   }
-
-//   const sql = "INSERT INTO person (name, role) VALUES (?, ?)";
-//   const params = [name, role || 0];
-
-//   db.run(sql, params, function (err) {
-//     if (err) {
-//       return res.status(500).json({ error: err.message });
-//     }
-//     res.status(201).json({
-//       message: "Person berhasil ditambahkan",
-//       data: {
-//         id: this.lastID,
-//         name,
-//         role,
-//       },
-//     });
-//   });
-// });
+app.get("/profile", (req, res) => {
+  return res.json({
+    status: true,
+    statusCode: 200,
+    data: profile,
+  });
+});
 
 app.get("/chat", async (req, res) => {
   try {
